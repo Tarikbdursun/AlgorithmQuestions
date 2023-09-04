311149d8395c5657da7eb0021778e78d6711c310
Tarık Buğra Dursun <78389710+Tarikbdursun@users.noreply.github.com>
2023-09-04 16:58:51 +0300

    +Added : Pair Adding

---
+++ PairAdding/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+numbersInputPoint:
+List<int> numbers = new();
+List<int> addedNumbers = new();
+
+Console.WriteLine("Please Give A Number Array With Spaces");
+string? numbersInput;
+numbersInput = Console.ReadLine();
+
+try
+{
+    string[] seperatedNumbers = numbersInput.Split(" ");
+
+    foreach (string number in seperatedNumbers)
+    {
+        try
+        {
+            int.Parse(number);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Please Try Again");
+            goto numbersInputPoint;
+        }
+    }
+
+    for (int i = 0; i < seperatedNumbers.Length; i++)
+    {
+        int currentValue = 0;
+        int nextValue = 0;
+        int result = 0;
+
+
+        if (i == seperatedNumbers.Length - 1)
+        {
+            result = int.Parse(seperatedNumbers[i]);
+            addedNumbers.Add(result);
+            break;
+        }
+
+        if (i % 2 == 0)
+        {
+            currentValue = int.Parse(seperatedNumbers[i]);
+            nextValue = int.Parse(seperatedNumbers[i + 1]);
+            result = currentValue + nextValue;
+            if (currentValue == nextValue)
+            {
+                result = (int)Math.Pow((currentValue * 2), 2);
+            }
+
+            addedNumbers.Add(result);
+        }
+    }
+
+    foreach (int addedNumber in addedNumbers)
+    {
+        Console.WriteLine(addedNumber);
+    }
+}
+catch
+{
+    goto numbersInputPoint;
+}
